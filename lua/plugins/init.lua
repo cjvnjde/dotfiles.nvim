f01c0b25b96c38598b9cfb6cf4f1f161d83544a5
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-03-26 16:38:29 +0200

    add neotest

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -116,6 +116,47 @@ return {
   },
 
   {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/neotest-jest",
+    },
+    config = function()
+      require("neotest").setup {
+        discovery = {
+          enabled = false,
+        },
+        adapters = {
+          require('neotest-jest')({
+            jest_test_discovery = false,
+            jestCommand = "npm test -- collectCoverage=true",
+            jestConfigFile = "jest.config.ts",
+            env = { CI = true },
+            cwd = function()
+              return vim.fn.getcwd()
+            end,
+          }),
+        }
+      }
+    end,
+  },
+
+  {
+    "andythigpen/nvim-coverage",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("coverage").setup({
+        auto_reload = true
+      })
+    end,
+  },
+
+  {
     'mikesmithgh/kitty-scrollback.nvim',
     enabled = true,
     lazy = true,
