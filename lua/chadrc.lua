84badff54595d6250efeff5481cd7e8b85735139
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-04-01 11:14:12 +0300

    update nvimtree config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,20 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "catppuccin",
+  theme = "catppuccin",
   theme_toggle = { "catppuccin", "catppuccin" },
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  tabufline = {
+    order = { "buffers", "tabs", "btns" },
+  },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M
