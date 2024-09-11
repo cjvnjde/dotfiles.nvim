97fabfb95270ff5d001e8c3a98680957e2aa62da
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-09-11 19:56:21 +0300

    feat: add mason and conform

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
+  -- theme
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -18,9 +19,24 @@ return {
     cmd = { "NvimTreeToggle", "NvimTreeFocus" },
     opts = (require "configs.nvimtree"),
   },
+  -- add/update/delete surrounding pairs
   {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
+    config = true,
+  },
+  -- auto tab width
+  "tpope/vim-sleuth",
+  -- install additional tools
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  -- formatting
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = (require "configs.conform"),
   },
 }
