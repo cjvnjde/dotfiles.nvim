bb1f6e8002698c9c74f959d06aa70573031339ea
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-11-18 19:55:16 +0200

    add jsonc formatter

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -8,6 +8,7 @@ return {
     css = { "prettier" },
     html = { "prettier" },
     json = { "prettier" },
+    jsonc = { "prettier" },
     yaml = { "prettier" },
     markdown = { "prettier" },
     graphql = { "prettier" },
