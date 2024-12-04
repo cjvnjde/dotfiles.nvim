50a2592c3c63d8b9631faf88665f1f688bef38fe
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-12-04 11:02:19 +0200

    chore: add the ability to remove harpoon pages

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -108,10 +108,37 @@ M.global = function()
   map("n", "<M-a>", function()
     harpoon:list():add()
   end)
+
+  map("n", "<M-d>", function()
+    harpoon:list():remove()
+  end)
+
+  local conf = require("telescope.config").values
+  local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers")
+      .new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table {
+          results = file_paths,
+        },
+        previewer = conf.file_previewer {},
+        sorter = conf.generic_sorter {},
+      })
+      :find()
+  end
+
+  map("n", "<leader>fh", function()
+    toggle_telescope(harpoon:list())
+  end, { desc = "[F]ind [H]arpoon" })
+
   map("n", "<M-e>", function()
     harpoon.ui:toggle_quick_menu(harpoon:list())
   end)
-
   map("n", "<M-h>", function()
     harpoon:list():select(1)
   end, { desc = "Go to Harpoon 1" })
