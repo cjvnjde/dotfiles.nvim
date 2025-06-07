769730929ef3441f072b8ad5bacf685cb5da262a
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2025-06-07 17:52:50 +0300

    fix(deps): update plugin versions in lazy-lock.json

--- lua/plugins/experiments.lua
+++ lua/plugins/experiments.lua
@@ -55,9 +55,7 @@ return {
     "ravitemer/mcphub.nvim",
     build = "npm install -g mcp-hub@latest",
     config = function()
-      require("mcphub").setup {
-        config = { auto_approve = true },
-      }
+      require("mcphub").setup()
     end,
   },
   {
