7852c9ccecf411f18b5d1fb0c90108865c87cf6b
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2025-11-30 17:41:09 +0200

    chore: update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
   -- Treesitter for enhanced syntax highlighting and indentation
   {
     "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs",
     opts = {
       ensure_installed = extensions.treesitter,
       auto_install = true,
